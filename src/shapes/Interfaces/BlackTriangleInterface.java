e7fb8afc43d5aa8180b29c625d4dc6ff6c2f14c2
PonyWings <fullFieldsOfHorsesUnderMoon>
2018-11-14 23:20:09 +0200

    test2

---
+++ src/shapes/Interfaces/BlackTriangleInterface.java
@@ -0,0 +1,8 @@
+package shapes.Interfaces;
+
+/**
+ * Created by karpenka on 14.11.2018.
+ */
+interface BlackTriangleInterface {
+    void printBlackCircleHeadPoint();//"im triangle white color with some main point in x, y"
+}
---
+++ src/shapes/Interfaces/WhiteTriangleInterface.java
@@ -0,0 +1,8 @@
+package shapes.Interfaces;
+
+/**
+ * Created by karpenka on 14.11.2018.
+ */
+interface WhiteTriangleInterface {
+    void printWhiteTrianglePerimetr(); //"im triangle white color with some perimetr"
+}
