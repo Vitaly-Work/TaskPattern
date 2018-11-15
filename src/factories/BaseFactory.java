c8fafd40e6dc713e51ec7108667bdc7131150b2a
Івахненко Володимир Олександрович <ivahnenko.v@np.ua>
2018-11-15 13:27:17 +0200

    added str name, getSet, toString to BaseFactory

--- src/factories/BaseFactory.java
+++ src/factories/BaseFactory.java
@@ -2,9 +2,23 @@ package factories;
 
 import shapes.*;
 
-
 public abstract class BaseFactory {
+	private String name;
 	
 	abstract Circle createCircle();
 	abstract Triangle createTriangle();
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "BaseFactory [name=" + name + "]";
+	}
 }
